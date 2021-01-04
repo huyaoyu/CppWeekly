c571cffb64b9024a438fb4c9e5ec4fe8384eaa0c
Yaoyu Hu <yaoyuh@andrew.cmu.edu>
2021-01-03 21:22:28 -0500

    Initial commit.

---
+++ 010_VariadicExpansion/main.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+template< typename ... T >
+std::vector<std::string> convert_2_str( const T& ... args ) {
+    std::stringstream ss;
+    std::vector<std::string> vs;
+    (void)std::initializer_list<int>{ 
+        ( ss.str(""), 
+          ss << args, 
+          vs.push_back(ss.str()), 
+          0 )... };
+    return vs;
+}
+
+int main( int argc, char** argv ) {
+    std::cout << "Hello, VariadicExpansion! \n";
+    
+    auto vs = convert_2_str( "Hello", "world", 1, 2.2f, 3.3 );
+
+    for ( const auto& s : vs ) {
+        std::cout << s << '\n';
+    }
+
+    std::cout << "Done! \n";
+
+    return 0;
+}
